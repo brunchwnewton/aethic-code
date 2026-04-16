4808ec20b13873312b2c760386b0035a1b79e759
brunchwnewton <brunchwnewton@gmail.com>
2026-04-16 02:14:59 -0400

    Delete files/timeline/js/descriptionLookup.js

--- files/timeline/js/descriptionLookup.js
+++
@@ -1,9 +0,0 @@
-function getMomentDescription(mya) {
-    if (mya>miracles[0][0]) {
-        return "The Earth coalesces from the raw material at the birth of the Solar System. It goes from a cloud of dust, growing more and more with each new particle, until it has become its full, planetary sized bulk of Essence.";
-    }else if (mya>miracles[1][0]) {
-        return "The Earth is recovering from its giant impact, now orbited by the baby form our moon. The Hadean wraps up and it appears as though the Solar System is settling down, upon the surface of this dried up rock.";
-    }else if (mya>miracles[2][0]) {
-        return "The Late Heavy Bombardment booms on in full force. Holes are blown into the surfaces of the planets as the Hadean deals its unexpected second blow. But all the while water accumulates..."
-    }
-}
